681676034fa09d716a748ba8470c48a803754176
Izabelle Datayan <i_datayan@yahoo.com>
2022-05-13 00:06:34 -0700

    fixed a bug

--- public/js/register.js
+++ public/js/register.js
@@ -57,7 +57,7 @@ ready(function () {
       let userName = document.getElementById("userName");
       let firstName = document.getElementById("firstName");
       let lastName = document.getElementById("lastName");
-      const vars = { "email": email.value, "password": password.value, "userName": userName.value, "firstName": firstName.value, "lastName": lastName.value, "isAdmin": isAdminVal }
+      const vars = { "email": email.value, "password": password.value, "userName": userName.value, "firstName": firstName.value, "lastName": lastName.value}
 
       ajaxPOST("/register", function (data) {
           if (data) {
