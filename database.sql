3b3a9238eb95809fc3c374c3365104c188eed087
Izabelle Datayan <i_datayan@yahoo.com>
2022-05-13 12:20:18 -0700

    updated the mysql code

--- database.sql
+++ database.sql
@@ -2,14 +2,15 @@ CREATE DATABASE IF NOT EXISTS COMP2800;
 use COMP2800;
 
 CREATE TABLE IF NOT EXISTS BBY_25_users (
-  ID int NOT NULL AUTO_INCREMENT,
+  identity int NOT NULL AUTO_INCREMENT,
   user_name varchar(30),
   first_name varchar(30),
   last_name varchar(30),
   email varchar(30),
   password varchar(30),
   is_admin BOOLEAN,
-  PRIMARY KEY (ID));
+  profile_pic varchar(60),
+  PRIMARY KEY (identity));
 
 CREATE TABLE IF NOT EXISTS BBY_25_users_packages (
   userID int NOT NULL,
@@ -20,12 +21,12 @@ CREATE TABLE IF NOT EXISTS BBY_25_users_packages (
   PRIMARY KEY(packageID),
   CONSTRAINT fk_category
   FOREIGN KEY (userID)
-    REFERENCES BBY_25_users(ID));
+    REFERENCES BBY_25_users(identity));
 
-INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin) VALUES ("dbui", "Devon", "Bui", "dbui@bcit.ca", "test3", false);
+INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin, profile_pic) VALUES ("dbui", "Devon", "Bui", "dbui@bcit.ca", "test3", false, "/img/luffy.png");
 
-INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin) VALUES ("damah", "David", "Amah", "damah@bcit.ca", "test4", false);
+INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin, profile_pic) VALUES ("damah", "David", "Amah", "damah@bcit.ca", "test4", false, "/img/luffy.png");
 
-INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin) VALUES ("pdychinco", "Princeton", "Dychinco", "pdychinco@bcit.ca", "test1", true);
+INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin, profile_pic) VALUES ("pdychinco", "Princeton", "Dychinco", "pdychinco@bcit.ca", "test1", true, "/img/luffy.png");
 
-INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin) VALUES ("idatayan", "Izabelle", "Datayan", "idatayan@bcit.ca", "test2", true);
+INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin, profile_pic) VALUES ("idatayan", "Izabelle", "Datayan", "idatayan@bcit.ca", "test2", true, "/img/luffy.png");
