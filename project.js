cd2a0f0da45657bd89af7d5ebcf08750940b7172
Princeton Dychinco <prdychinco@gmail.com>
2022-05-14 10:17:32 -0700

    rendered catalogue items to client

--- project.js
+++ project.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 const express = require("express");
 const session = require("express-session");
 const app = express();
 const fs = require("fs");
 const { connect } = require("http2");
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require("jsdom");
 
 // static path mappings
 app.use("/js", express.static("./public/js"));
@@ -14,45 +14,38 @@ app.use("/fonts", express.static("./public/fonts"));
 app.use("/html", express.static("./public/html"));
 app.use("/media", express.static("./public/media"));
 
-
 //session connection
-app.use(session(
-  {
-      secret: "extra text that no one will guess",
-      name: "wazaSessionID",
-      resave: false,
-      // create a unique identifier for that client
-      saveUninitialized: true
+app.use(
+  session({
+    secret: "extra text that no one will guess",
+    name: "wazaSessionID",
+    resave: false,
+    // create a unique identifier for that client
+    saveUninitialized: true,
   })
 );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.get("/", function (req, res) {
   if (req.session.loggedIn) {
-      res.redirect("/profile");
+    res.redirect("/profile");
   } else {
+    let doc = fs.readFileSync("./app/html/login.html", "utf8");
 
-      let doc = fs.readFileSync("./app/html/login.html", "utf8");
-
-      res.set("Server", "Wazubi Engine");
-      res.set("X-Powered-By", "Wazubi");
-      res.send(doc);
+    res.set("Server", "Wazubi Engine");
+    res.set("X-Powered-By", "Wazubi");
+    res.send(doc);
   }
 });
 
-app.get("/register", function (req,res) {
-  let doc = fs.readFileSync("./app/html/register.html","utf8");
-  res.send(doc)
-})
+app.get("/register", function (req, res) {
+  let doc = fs.readFileSync("./app/html/register.html", "utf8");
+  res.send(doc);
+});
 
-app.get("/package", function (req,res) {
-  let doc = fs.readFileSync("./app/html/catalogue.html","utf8");
-  let docDOM = new JSDOM(doc);
-
+app.get("/get-catalogue", function(req, res) {
   const mysql = require("mysql2");
   const connection = mysql.createConnection({
     host: "127.0.0.1",
@@ -61,41 +54,70 @@ app.get("/package", function (req,res) {
     multipleStatements: "true"
   });
   connection.connect();
-
-  connection.query(`use COMP2800; select * from BBY_25_catalogue;`, function (error, results, fields) {
-    let items = results[1];
-    console.log(items[0]["itemID"]);
-    let contentSelector = docDOM.window.document;
-    console.log(items.length);
-    for (let i = 0; i < items.length; i++) {
-      let j = i++;
-      let cardID = contentSelector.getElementById(`item${j}ID`);
-      let cardName = contentSelector.getElementById(`item${j}Name`);
-      let cardQuantity = contentSelector.getElementById(`item${j}Quantity`);
-      let cardPrice = contentSelector.getElementById(`item${j}Price`);
-      let cardMostWanted = contentSelector.getElementById(`item${j}MostWanted`);
-      cardID.innerHTML = items[i]["itemID"];
-      cardName.innerHTML = items[i].name;
-      cardQuantity.innerHTML = items[i].quantity;
-      cardPrice.innerHTML = items[i].price;
-      cardMostWanted.innerHTML = items[i].most_wanted;
+  connection.query("use comp2800; select * from bby_25_catalogue;",
+  function (error, results, fields) {
+    if(error) {
+      //catch error and save to database
+    } else {
+      res.send({ status: "success", rows: results[1]});
     }
   })
-  res.set("Server", "Wazubi Engine");
-  res.set("X-Powered-By", "Wazubi");
-  res.send(docDOM.serialize());
   connection.end();
-})
+});
+
+app.get("/package", function (req, res) {
+  let doc = fs.readFileSync("./app/html/catalogue.html", "utf8");
+  // let docDOM = new JSDOM(doc);
+
+  // const mysql = require("mysql2");
+  // const connection = mysql.createConnection({
+  //   host: "127.0.0.1",
+  //   user: "root",
+  //   password: "",
+  //   multipleStatements: "true",
+  // });
+  // connection.connect();
+
+  // connection.query(
+  //   `use COMP2800; select * from BBY_25_catalogue;`,
+  //   function (error, results, fields) {
+  //     let items = results[1];
+  //     console.log(items[0]["itemID"]);
+  //     let contentSelector = docDOM.window.document;
+  //     console.log(items.length);
+  //     for (let i = 0; i < items.length; i++) {
+  //       let j = i++;
+  //       let cardID = contentSelector.getElementById(`item${j}ID`);
+  //       let cardName = contentSelector.getElementById(`item${j}Name`);
+  //       let cardQuantity = contentSelector.getElementById(`item${j}Quantity`);
+  //       let cardPrice = contentSelector.getElementById(`item${j}Price`);
+  //       let cardMostWanted = contentSelector.getElementById(
+  //         `item${j}MostWanted`
+  //       );
+  //       cardID.innerHTML = items[i]["itemID"];
+  //       cardName.innerHTML = items[i].name;
+  //       cardQuantity.innerHTML = items[i].quantity;
+  //       cardPrice.innerHTML = items[i].price;
+  //       cardMostWanted.innerHTML = items[i].most_wanted;
+  //     }
+  //   }
+  // );
+  // res.set("Server", "Wazubi Engine");
+  // res.set("X-Powered-By", "Wazubi");
+  // res.send(docDOM.serialize());
+  // connection.end();
+  res.send(doc);
+});
 
 app.get("/profile", function (req, res) {
   // Check if user properly authenticated and logged in
   if (req.session.loggedIn) {
     if (req.session.userType) {
-    let profile = fs.readFileSync("./app/html/adminProfile.html", "utf8");
-    let profileDOM = new JSDOM(profile);
+      let profile = fs.readFileSync("./app/html/adminProfile.html", "utf8");
+      let profileDOM = new JSDOM(profile);
 
-      profileDOM.window.document.getElementById("profile_name").innerHTML
-          = "Welcome back " + req.session.name;
+      profileDOM.window.document.getElementById("profile_name").innerHTML =
+        "Welcome back " + req.session.name;
 
       res.set("Server", "Wazubi Engine");
       res.set("X-Powered-By", "Wazubi");
@@ -103,35 +125,43 @@ app.get("/profile", function (req, res) {
     } else {
       let profile = fs.readFileSync("./app/html/profile.html", "utf8");
       let profileDOM = new JSDOM(profile);
-  
-      profileDOM.window.document.getElementById("profile_name").innerHTML
-          = "Welcome back " + req.session.name;
-  
+
+      profileDOM.window.document.getElementById("profile_name").innerHTML =
+        "Welcome back " + req.session.name;
+
       res.set("Server", "Wazubi Engine");
       res.set("X-Powered-By", "Wazubi");
       res.send(profileDOM.serialize());
     }
   } else {
-      // not logged in - no session and no access, redirect to home!
-      res.redirect("/");
+    // not logged in - no session and no access, redirect to home!
+    res.redirect("/");
   }
 });
 
-app.post("/register", function(req, res) {
+app.post("/register", function (req, res) {
   res.setHeader("Content-Type", "application/json");
   const mysql = require("mysql2");
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  multipleStatements: "true"
-});
+  const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    multipleStatements: "true",
+  });
   connection.connect();
 
   let validNewUserInfo = req.body;
   //Adds new user to user table. Always non admin, since this is client facing sign up
-  connection.query(`use COMP2800; INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin) values (?, ?, ?, ?, ?, ?)`, 
-    [validNewUserInfo.userName, validNewUserInfo.firstName, validNewUserInfo.lastName, validNewUserInfo.email, validNewUserInfo.password, false],
+  connection.query(
+    `use COMP2800; INSERT INTO BBY_25_users (user_name, first_name, last_name, email, password, is_admin) values (?, ?, ?, ?, ?, ?)`,
+    [
+      validNewUserInfo.userName,
+      validNewUserInfo.firstName,
+      validNewUserInfo.lastName,
+      validNewUserInfo.email,
+      validNewUserInfo.password,
+      false,
+    ],
     function (error, results, fields) {
       if (error) {
         // send error to DB
@@ -144,62 +174,61 @@ const connection = mysql.createConnection({
       req.session.userType = validNewUserInfo.is_admin;
       req.session.save(function (err) {
         // session saved. for analytics we could record this in db
-      })
+      });
       res.send({ status: "success", msg: "Record added." });
-
-    });
+    }
+  );
   connection.end();
-})
+});
 
 app.post("/login", function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    const mysql = require("mysql2");
+  res.setHeader("Content-Type", "application/json");
+  const mysql = require("mysql2");
   const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "",
-    multipleStatements: "true"
+    multipleStatements: "true",
   });
-    
-    connection.connect();
-    // Checks if user typed in matching email and password
-    const loginInfo = `USE COMP2800; SELECT * FROM BBY_25_USERS WHERE email = '${req.body.email}' AND password = '${req.body.password}';`;
-    connection.query(loginInfo, function (error, results, fields) {
-      /* If there is an error, alert user of error
-      *  If the length of results array is 0, then there was no matches in database
-      *  If no error, then it is valid login and save info for session
-      */ 
-      if (error) {
-        // change this to notify user of error
-      } else if (results[1].length == 0) {
-        res.send({ status: "fail", msg: "Incorrect email or password"});
-      }else {
-        let validUserInfo = results[1][0];
-        req.session.loggedIn = true;
-        req.session.email = validUserInfo.email;
-        req.session.name = validUserInfo.first_name;
-        req.session.identity = validUserInfo.ID;
-        req.session.userType = validUserInfo.is_admin;
-        req.session.save(function (err) {
+
+  connection.connect();
+  // Checks if user typed in matching email and password
+  const loginInfo = `USE COMP2800; SELECT * FROM BBY_25_USERS WHERE email = '${req.body.email}' AND password = '${req.body.password}';`;
+  connection.query(loginInfo, function (error, results, fields) {
+    /* If there is an error, alert user of error
+     *  If the length of results array is 0, then there was no matches in database
+     *  If no error, then it is valid login and save info for session
+     */
+    if (error) {
+      // change this to notify user of error
+    } else if (results[1].length == 0) {
+      res.send({ status: "fail", msg: "Incorrect email or password" });
+    } else {
+      let validUserInfo = results[1][0];
+      req.session.loggedIn = true;
+      req.session.email = validUserInfo.email;
+      req.session.name = validUserInfo.first_name;
+      req.session.identity = validUserInfo.ID;
+      req.session.userType = validUserInfo.is_admin;
+      req.session.save(function (err) {
         // session saved. for analytics we could record this in db
-      })
-      res.send({ status: "success", msg: "Logged in."});
-      }
-    })
+      });
+      res.send({ status: "success", msg: "Logged in." });
+    }
+  });
   connection.end();
 });
 
 app.get("/logout", function (req, res) {
-
   if (req.session) {
-      req.session.destroy(function (error) {
-          if (error) {
-              res.status(400).send("Unable to log out")
-          } else {
-              // session deleted, redirect to home
-              res.redirect("/");
-          }
-      });
+    req.session.destroy(function (error) {
+      if (error) {
+        res.status(400).send("Unable to log out");
+      } else {
+        // session deleted, redirect to home
+        res.redirect("/");
+      }
+    });
   }
 });
 
--- public/js/catalogue.js
+++ public/js/catalogue.js
@@ -1,63 +1,94 @@
-'use strict';
+"use strict";
 ready(function () {
-
   function ajaxGET(url, callback) {
-
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-          if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-              callback(this.responseText);
-
-          }
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        callback(this.responseText);
       }
-      xhr.open("GET", url);
-      xhr.send();
+    };
+    xhr.open("GET", url);
+    xhr.send();
   }
 
   function ajaxPOST(url, callback, data) {
+    /*
+     * - Keys method of the object class returns an array of all of the keys for an object
+     * - The map method of the array type returns a new array with the values of the old array
+     *   and allows a callback function to perform an action on each key
+     *   The join method of the arra type accepts an array and creates a string based on the values
+     *   of the array, using '&' we are specifying the delimiter
+     * - The encodeURIComponent function escapes a string so that non-valid characters are replaced
+     *   for a URL (e.g., space character, ampersand, less than symbol, etc.)
+     *
+     *
+     * References:
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+     */
+    let params =
+      typeof data == "string"
+        ? data
+        : Object.keys(data)
+            .map(function (k) {
+              return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+            })
+            .join("&");
 
-      /*
-       * - Keys method of the object class returns an array of all of the keys for an object
-       * - The map method of the array type returns a new array with the values of the old array
-       *   and allows a callback function to perform an action on each key
-       *   The join method of the arra type accepts an array and creates a string based on the values
-       *   of the array, using '&' we are specifying the delimiter
-       * - The encodeURIComponent function escapes a string so that non-valid characters are replaced
-       *   for a URL (e.g., space character, ampersand, less than symbol, etc.)
-       *
-       *
-       * References:
-       * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
-       * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-       * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
-       */
-      let params = typeof data == 'string' ? data : Object.keys(data).map(
-          function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-      ).join('&');
-
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-          if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-              callback(this.responseText);
-
-          }
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        callback(this.responseText);
       }
-      xhr.open("POST", url);
-      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.send(params);
+    };
+    xhr.open("POST", url);
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(params);
   }
 
+  function getItems() {
+    ajaxGET("/get-catalogue", function (data) {
+      let parsedData = JSON.parse(data);
+      console.log(`this is parsed: ${parsedData}`);
+      if (parsedData.status == "success") {
+        let str = `<tr>
+          <th class="id_header"><span>ID</span></th>
+          <th class="name_header"><span>Name</span></th>
+          <th class="quantity_header"><span>Quantity</span></th>
+          <th class="price_header"><span>Price</span></th>
+          <th class="mostWanted_header"><span>Most Wanted</span></th>
+          </tr>`;
 
-
+        for (let i = 0; i < parsedData.rows.length; i++) {
+          let row = parsedData.rows[i];
+          console.log(`new row: ${row}`);
+          str +=
+            "<tr><td class='id'>" +
+            row.itemID +
+            "</td><td class='name'><span>" +
+            row.name +
+            "</span></td><td class='quantity'><span>" +
+            row.quantity +
+            "</span></td><td class='price'><span>" +
+            row.price +
+            "</span>" +
+            "</td><td class='most_wanted'><span>" +
+            row.most_wanted +
+            "</span></td></tr>";
+        }
+        document.getElementById("content").innerHTML = str;
+      }
+    });
+  }
+  getItems();
 });
 
 function ready(callback) {
   if (document.readyState != "loading") {
-      callback();
+    callback();
   } else {
-      document.addEventListener("DOMContentLoaded", callback);
+    document.addEventListener("DOMContentLoaded", callback);
   }
 }
-
-const element = document.getElementById("content");
